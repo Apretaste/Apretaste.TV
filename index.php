af93439fd1dbbbbeb728630e1004a627a6c2f04a
Salvi Pascual <salvi.pascual@gmail.com>
2021-02-08 16:16:37 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Apretaste.TV</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Apretaste.TV te trae videos de todo lo que te interesa: deporte, noticias, política, tecnología, inversión y mucho más, hecho por nuestra comunidad de Apretadores.">
+	<meta name="author" content="Apretaste">
+	<link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+	<script type="text/javascript">
+		setTimeout(function(){
+			<?php if(empty($_GET['q'])) { ?>
+				window.location = 'https://www.youtube.com/apretaste';
+			<?php } else { ?>
+				window.location = 'https://www.youtube.com/watch?v=<?= $_GET['q'] ?>';
+			<?php } ?>
+		}, 1000);
+	</script>
+</head>
+<body style="background:black url('loading.gif') no-repeat fixed center;">
+	Cargando Apretaste.TV
+</body>
+</html>
