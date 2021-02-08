460e25df49fc8176fb9795550175431642ef2269
Salvi Pascual <salvi.pascual@gmail.com>
2021-02-08 16:37:28 -0500

    add google verification

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<meta name="description" content="Apretaste.TV te trae videos de todo lo que te interesa: deporte, noticias, política, tecnología, inversión y mucho más, hecho por nuestra comunidad de Apretadores.">
 	<meta name="author" content="Apretaste">
 	<link rel="icon" type="image/x-icon" href="/favicon.ico">
+	<meta name="google-site-verification" content="LFSeXxsA8pm8OSQXPMoad9BS9LkdaLS1Qm5zc5ijApA" />
 
 	<script type="text/javascript">
 		setTimeout(function(){
